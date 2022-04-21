9d5fad8b54853c20eb75859feda22df214ee1334
jdidcote <joshua.didcote@outlook.com>
2022-04-21 20:58:15 +0100

    Added basic calculator layout and operations

---
+++ calculator/app.js
@@ -0,0 +1,26 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(x, y, operation) {
+  return operation(x, y);
+}
+
+// const test = document.querySelectorAll(".calc-button");
+// test.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     console.log(button);
+//   });
+// });
