2f8985084a30df80f4c9726720371ffcfb986ac3
jdidcote <joshua.didcote@outlook.com>
2022-04-26 15:50:31 +0100

    Base calculator functionality working

--- calculator/app.js
+++ calculator/app.js
@@ -1,26 +1,108 @@
-function add(x, y) {
-  return x + y;
+function calculate(a, b, operation) {
+  a = parseInt(a);
+  b = parseInt(b);
+
+  let calculation;
+
+  switch (operation) {
+    case "+":
+      calculation = a + b;
+      break;
+    case "-":
+      calculation = a - b;
+      break;
+    case "*":
+      calculation = a * b;
+      break;
+    case "/":
+      calculation = a / b;
+      break;
+  }
+  return calculation;
 }
 
-function subtract(x, y) {
-  return x - y;
+var inputArea = document.querySelector(".number-area");
+var resultArea = document.querySelector(".result-area");
+
+function updateResultArea(result) {
+  resultArea.innerHTML = result;
 }
 
-function multiply(x, y) {
-  return x * y;
+class Calculation {
+  constructor() {
+    this.resetNumbers();
+    this.resetResult();
+    this.operators = ["+", "-", "*", "/"];
+    this.numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  }
+
+  resetNumbers() {
+    this.firstNumber = "";
+    this.operation = "";
+    this.secondNumber = "";
+  }
+
+  resetResult() {
+    this.currentResult = 0;
+  }
+
+  checkShouldCalculate(newInput) {
+    const shouldCalculate = this.secondNumber != "" && newInput == "=";
+    return shouldCalculate;
+  }
+
+  calculate() {
+    const result = calculate(
+      this.firstNumber,
+      this.secondNumber,
+      this.operation
+    );
+    this.resetNumbers();
+    this.currentResult = result;
+    updateResultArea(result);
+  }
+
+  addnewInput(newInput) {
+    if (this.checkShouldCalculate(newInput)) {
+      this.calculate();
+      return;
+    }
+
+    // First number
+    if (this.operation == "" && this.numbers.includes(newInput)) {
+      this.firstNumber += newInput;
+    }
+
+    // Operation
+    if (this.operators.includes(newInput) && this.secondNumber == "") {
+      this.operation = newInput;
+    }
+
+    // Second number
+    if (this.operation != "" && this.numbers.includes(newInput)) {
+      this.secondNumber += newInput;
+    }
+  }
+
+  displayCalculation() {
+    return this.firstNumber + " " + this.operation + " " + this.secondNumber;
+  }
 }
 
-function divide(x, y) {
-  return x / y;
+const calculation = new Calculation();
+
+function updateinputArea(newInput, resetCalculation = false) {
+  if (resetCalculation) {
+    const calculation = new Calculation();
+  }
+
+  calculation.addnewInput(newInput);
+  inputArea.innerHTML = calculation.displayCalculation();
 }
 
-function operate(x, y, operation) {
-  return operation(x, y);
-}
-
-// const test = document.querySelectorAll(".calc-button");
-// test.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     console.log(button);
-//   });
-// });
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    updateinputArea(button.textContent.trim());
+  });
+});
